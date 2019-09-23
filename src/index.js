058fd856a4995bb5729dcff93b0c177a692de5d9
Oleg Smirnov <sovaz1997@yandex.ru>
2019-09-23 09:28:30 +0300

    solved for some cases

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+ERROR = -1
+OPENED = 0;
+CLOSED = 1;
+
+function detectBracket(bracket, bracketsConfig) { 
+    for(let i = 0; i < bracketsConfig.length; i++) {
+        if(bracket == bracketsConfig[i][0]) {
+            return { 'position': i, 'type': OPENED };
+        } else if(bracket == bracketsConfig[i][1]) {
+            return { 'position': i, 'type': CLOSED };
+        }
+    }
+
+    return { 'position': -1, 'type': ERROR }; 
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    console.log(str, bracketsConfig);
+
+    let stack = [];
+
+    for(let i = 0; i < str.length; i++) {
+        bracket = detectBracket(str[i], bracketsConfig);
+
+        if(bracket.type == OPENED) {
+            stack.push(bracket.position);
+        } else if(bracket.type == CLOSED) {
+            if(stack.length && bracket.position == stack[stack.length - 1]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return stack.length == 0;
 }
